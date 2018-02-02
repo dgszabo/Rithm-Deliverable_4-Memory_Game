03100b924a571105e6b1ba1b4c3481f4c938ecf5
Daniel Szabo <dgszabo@gmail.com>
2018-02-01 16:54:40 -0800

    First test version - with hardcoded elements, for logic testing

--- memory.js
+++ memory.js
@@ -1,3 +1,11 @@
+var statusStorage = { score: 0 }
+
 document.addEventListener("DOMContentLoaded", function(){
-	console.log('Im connected!')
+	var imageBoxes = document.querySelectorAll('.imgBox');
+
+	for(var i = 0; i < imageBoxes.length; i++) {
+		imageBoxes[i].addEventListener('click', function() {
+			console.log('You clicked ' + this.firstElementChild.src);
+		})
+	}
 });
